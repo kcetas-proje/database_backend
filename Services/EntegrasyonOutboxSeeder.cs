7770bcf73a8f13cabe68bcdc74a0fd3e8f990c3c
dilaoztrk <162128427+dilaoztrk@users.noreply.github.com>
2026-07-24 14:37:22 +0300

    Create EntegrasyonOutboxSeeder.cs

---
+++ Services/EntegrasyonOutboxSeeder.cs
@@ -0,0 +1,109 @@
+using Bogus;
+using KcetasAboneApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KcetasSeeder.Seeders;
+
+public class EntegrasyonOutboxSeeder
+{
+    private readonly AppDbContext _context;
+
+    public EntegrasyonOutboxSeeder(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task SeedAsync(int adet = 500)
+    {
+        if (await _context.EntegrasyonOutboxes.AnyAsync())
+            return;
+
+        var faturalar = await _context.Faturas.ToListAsync();
+
+        if (!faturalar.Any())
+        {
+            Console.WriteLine("Fatura bulunamadığı için EntegrasyonOutbox oluşturulamadı.");
+            return;
+        }
+
+        var faker = new Faker<EntegrasyonOutbox>("tr")
+            .RuleFor(x => x.HedefSistem, f =>
+            {
+                var rnd = f.Random.Int(1, 100);
+
+                if (rnd <= 45)
+                    return HedefSistem.ERP;
+
+                if (rnd <= 70)
+                    return HedefSistem.GIB_EFATURA;
+
+                if (rnd <= 85)
+                    return HedefSistem.GIB_EARSIV;
+
+                if (rnd <= 95)
+                    return HedefSistem.CRM_NOTIFICATION;
+
+                return HedefSistem.FIELD_PRINT;
+            })
+            .RuleFor(x => x.Durum, f =>
+            {
+                var rnd = f.Random.Int(1, 100);
+
+                if (rnd <= 70)
+                    return OutboxDurumu.GONDERILDI;
+
+                if (rnd <= 85)
+                    return OutboxDurumu.BEKLIYOR;
+
+                if (rnd <= 95)
+                    return OutboxDurumu.HATA;
+
+                return OutboxDurumu.MANUEL_MUDAHALE;
+            });
+            
+        var liste = faker.Generate(adet);
+
+        foreach (var item in liste)
+        {
+            switch (item.Durum)
+            {
+                case OutboxDurumu.GONDERILDI:
+                    item.RetryCount = 0;
+                    item.GonderimZamani = item.CreatedAt.AddMinutes(Random.Shared.Next(1, 30));
+                    item.SonDenemeTarihi = item.GonderimZamani;
+                    item.HataKodu = null;
+                    item.HataMesaji = null;
+                    break;
+
+                case OutboxDurumu.BEKLIYOR:
+                    item.RetryCount = 0;
+                    item.GonderimZamani = null;
+                    item.SonDenemeTarihi = null;
+                    item.HataKodu = null;
+                    item.HataMesaji = null;
+                    break;
+
+                case OutboxDurumu.HATA:
+                    item.RetryCount = Random.Shared.Next(1, 6);
+                    item.SonDenemeTarihi = item.CreatedAt.AddMinutes(Random.Shared.Next(5, 120));
+                    item.GonderimZamani = null;
+                    item.HataKodu = "HTTP500";
+                    item.HataMesaji = "Hedef sisteme gönderim başarısız.";
+                    break;
+
+                case OutboxDurumu.MANUEL_MUDAHALE:
+                    item.RetryCount = Random.Shared.Next(3, 10);
+                    item.SonDenemeTarihi = item.CreatedAt.AddMinutes(Random.Shared.Next(30, 180));
+                    item.GonderimZamani = null;
+                    item.HataKodu = "MANUAL";
+                    item.HataMesaji = "Kayıt manuel inceleme bekliyor.";
+                    break;
+            }
+        }
+
+        await _context.EntegrasyonOutboxes.AddRangeAsync(liste);
+        await _context.SaveChangesAsync();
+
+        Console.WriteLine($"{liste.Count} adet EntegrasyonOutbox oluşturuldu.");
+    }
+}
